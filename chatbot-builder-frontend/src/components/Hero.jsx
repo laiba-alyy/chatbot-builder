240ca215789240ce707a0072392b5465f6d23ac1
laiba-alyy <147967396+laiba-alyy@users.noreply.github.com>
2026-04-11 18:31:06 +0500

    Update Hero.jsx

--- chatbot-builder-frontend/src/components/Hero.jsx
+++ chatbot-builder-frontend/src/components/Hero.jsx
@@ -30,8 +30,7 @@ const Hero = () => {
             <span className="cursor">|</span>
           </h1>
           <p>
-            Build Complex AI Agents in Minutes, Not days!
-            The No-Code Engine for High-Performance Industry AI. Build chatbots for your businesses and webistes in minutes without any coding 
+            Build Powerful AI Agents in Minutes — No Coding Required
           </p>
           <div className="hero-btns">
             <button className="btn-main">Get Started Free</button>
